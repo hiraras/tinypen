edc5560004c4bb81069144f2eb16db3f5903f3df
hirara <kazilagui@163.com>
2023-04-28 16:55:24 +0800

    feat: 补充类型

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -2,13 +2,12 @@ const getDetailByType = (data: any, jsonSpace = 4) => {
   switch (typeof data) {
     case "object":
       return JSON.stringify(data, null, jsonSpace);
-    case "function":
-      return data.toString();
     case "undefined":
       return "undefined";
-    case "undefined":
-      return "null";
+    case "bigint":
+      return `${data}n`;
     default:
+      // number、string、Symbol、function、boolean
       return data.toString();
   }
 };
